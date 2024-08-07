b6203206b1b100fcc71785a2c3710fba6dde82fb
Ryan Montgomery <coap.ryanmontgomery@gmail.com>
2024-08-07 05:36:46 +0000

    done

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 // index.js
-const hqStreet = 42;
-const blockLengthInFeet = 264;
+const feetPerBlock = 264
+const scuberHeadquarters = 42
 
-function distanceFromHqInBlocks(pickupStreet) {
-  return Math.abs(pickupStreet - hqStreet);
-}
+function distanceFromHqInBlocks(pickupLocation){
+    return Math.abs(pickupLocation - scuberHeadquarters)
+  };
 
-function distanceFromHqInFeet(pickupStreet) {
-  return distanceFromHqInBlocks(pickupStreet) * blockLengthInFeet;
-}
+function distanceFromHqInFeet(pickupLocation) {
+   return (distanceFromHqInBlocks(pickupLocation)*feetPerBlock)
+    }
 
-function distanceTravelledInFeet(start, destination) {
-  return Math.abs(start - destination) * blockLengthInFeet;
-}
+function distanceTravelledInFeet(pickupLocation, destination) {
+ return   Math.abs(distanceFromHqInFeet(pickupLocation) - distanceFromHqInFeet(destination))
+};
 
-function calculatesFarePrice(start, destination) {
-  const distance = distanceTravelledInFeet(start, destination);
-
-  if (distance <= 400) {
-    return 0;
-  } else if (distance > 400 && distance <= 2000) {
-    return (distance - 400) * 0.02;
-  } else if (distance > 2000 && distance <= 2500) {
-    return 25;
-  } else {
-    return 'cannot travel that far';
-  }
-}
+function calculatesFarePrice(pickupLocation, destination) {
+  if (distanceTravelledInFeet(pickupLocation, destination) <= 400) {
+    return parseInt(0)
+  };
+  if (distanceTravelledInFeet(pickupLocation, destination) <= 2000) {
+      return  parseFloat((distanceTravelledInFeet(pickupLocation, destination)-400)*(2/100))
+  };
+  if (distanceTravelledInFeet(pickupLocation, destination) <= 2500) {
+      return 25
+  } else
+  {
+      return 'cannot travel that far'
+      }
+} 
